dd2e31aae407d83112cdafdefae53916e8967cf0
Dadepo Aderemi <dadepo@gmail.com>
2021-04-23 05:17:20 +0200

    seperate env

--- utils/tweeter.ts
+++ utils/tweeter.ts
@@ -2,14 +2,20 @@ import twitter  from 'https://dev.jspm.io/twitter';
 
 import {config}  from "../deps.ts";
 
-const secret = {
-    consumer_key: config()["CONSUMER_KEY"],
-    consumer_secret: config()["CONSUMER_SECRET"],
-    access_token_key: config()["ACCESS_TOKEN_KEY"],
-    access_token_secret: config()["ACCESS_TOKEN_SECRET"]
-};
+let secret = {}
+let client: any = {}
 
-const client = (twitter as Function)(secret);
+if (config()["CONSUMER_KEY"] === "prod") {
+    secret = {
+        consumer_key: config()["CONSUMER_KEY"],
+        consumer_secret: config()["CONSUMER_SECRET"],
+        access_token_key: config()["ACCESS_TOKEN_KEY"],
+        access_token_secret: config()["ACCESS_TOKEN_SECRET"]
+    };
+
+    client = (twitter as Function)(secret);
+}
+
 const origin = new URL(config()["RESET_LINK"]).origin
 
 export const postTweet = (input: {title: string, url: string, uuid: string}) => {
@@ -21,10 +27,15 @@ export const postTweet = (input: {title: string, url: string, uuid: string}) => 
     Link: ${input.url}
     Discuss: ${discussUrl}
     `
-    client.post('statuses/update', {status: tweet}, (error: any, tweet: any, response: any) => {
-        if (error) throw error;
-        console.log(tweet);
-        console.log(response);
-    });
+    if (config()["CONSUMER_KEY"] === "prod") {
+        client.post('statuses/update', {status: tweet}, (error: any, tweet: any, response: any) => {
+            if (error) throw error;
+            console.log(tweet);
+            console.log(response);
+        });
+    } else {
+        console.log(tweet)
+    }
+
 }
 
