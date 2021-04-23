63a053a73f3d911e280bd08959c0262f331a498a
Dadepo Aderemi <dadepo@gmail.com>
2021-04-23 11:50:57 +0200

    fix dying

--- workers/poll_rss.ts
+++ workers/poll_rss.ts
@@ -7,13 +7,59 @@ import { db } from "../dao/db_connection.ts"
 import { Result } from "../lib.ts";
 import {config}  from "../deps.ts";
 import {isRelevant} from "../utils/patterns.ts";
-import {postTweet} from "../utils/tweeter.ts";
+import twitter from 'https://dev.jspm.io/twitter';
 
 const relevantPostDao = new RelevantPostDao(db)
 const rssLinkDao = new RssLinkDao(db)
-
-
 const domParser = new DOMParser()
+
+let secret = {}
+let client: any = {}
+
+if (config()["ENV"] === "prod") {
+    secret = {
+        consumer_key: config()["CONSUMER_KEY"],
+        consumer_secret: config()["CONSUMER_SECRET"],
+        access_token_key: config()["ACCESS_TOKEN_KEY"],
+        access_token_secret: config()["ACCESS_TOKEN_SECRET"]
+    };
+
+    client = (twitter as Function)(secret);
+}
+
+const origin = new URL(config()["RESET_LINK"]).origin
+
+export const postTweet = (input: {title: string, url: string, uuid: string}) => {
+
+    let discussUrl = `${origin}/${new URL(input.url).hostname}/?itemid=${input.uuid}`
+
+    let tweet = `
+    ${input.title}
+    
+   Link: ${input.url}
+   Discuss: ${discussUrl}
+    `
+    let hashTags = `
+    #typescript #100DaysOfCode #javascript
+    `
+    if ((tweet + hashTags).length <= 280) {
+        tweet = tweet + hashTags;
+    }
+
+    if (config()["ENV"] === "prod") {
+        console.log("In prod, so tweeting")
+        client.post('statuses/update', {status: tweet}, (error: any, tweet: any, response: any) => {
+            if (error) throw error;
+            console.log(tweet);
+            console.log(response);
+        });
+    } else {
+        console.log(tweet)
+    }
+
+}
+
+
 
 
 async function wait(ms: number) {
@@ -88,7 +134,7 @@ const poll_rss_link = async (website:string, rssLink: string) => {
                         if (result.value === 0) {
                             let uuid = relevantPostDao.savePost(website, url, title, doc.textContent).value!
 
-                            // postTweet({title, url, uuid})
+                            postTweet({title, url, uuid})
 
                         } else {
                             // console.log("already saved", url)
