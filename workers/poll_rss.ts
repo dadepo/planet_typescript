a67ade63de906f58193198b2de9622ba504d35d3
Dadepo Aderemi <dadepo@gmail.com>
2021-04-23 11:24:34 +0200

    don't die when you can't process a link

--- workers/poll_rss.ts
+++ workers/poll_rss.ts
@@ -38,71 +38,76 @@ type Item = {title: string, summary: string, url: string}
 
 const poll_rss_link = async (website:string, rssLink: string) => {
     const xml = await fetch(rssLink).then(resp => resp.text())
-    const { feed, feedType } = await deserializeFeed(xml);
 
-    let items: Item[] = []
+    try {
+        const {feed, feedType} = await deserializeFeed(xml);
 
-    if (isAtom(feed)) {
-        feed.entries.forEach(element => {
-            items.push({
-                title: element.title.value ?? "",
-                summary: element.summary?.value ?? "",
-                url: (element as any).href ?? ""
+        let items: Item[] = []
+
+        if (isAtom(feed)) {
+            feed.entries.forEach(element => {
+                items.push({
+                    title: element.title.value ?? "",
+                    summary: element.summary?.value ?? "",
+                    url: (element as any).href ?? ""
+                })
+            });
+        } else if (isRss1(feed)) {
+            feed.channel.items.forEach(element => {
+                items.push({
+                    title: element.title ?? "",
+                    summary: element.description ?? "",
+                    url: element.link ?? ""
+                })
             })
-        });
-    } else if (isRss1(feed)) {
-        feed.channel.items.forEach(element => {
-            items.push({
-                title: element.title ?? "",
-                summary: element.description ?? "",
-                url: element.link ?? ""
+        } else if (isRss2(feed)) {
+            feed.channel.items.forEach(element => {
+                items.push({
+                    title: element.title ?? "",
+                    summary: element.description ?? "",
+                    url: element.link ?? ""
+                })
             })
-        })
-    } else if (isRss2(feed)) {
-        feed.channel.items.forEach(element => {
-            items.push({
-                title: element.title ?? "",
-                summary: element.description ?? "",
-                url: element.link ?? ""
-            })
-        })
-    } else {
-        console.log(4, feedType)
-    }
-
-    for (const item of items) {
-        let title = item.title
-        let summary = item.summary
-        let url = item.url
-
-        let doc: any = domParser.parseFromString(summary, "text/html")
-        let relevant = isRelevant(title) || isRelevant(doc.textContent)
-        // TODO first extract all the url and check in one go, instead of one by one
-        let result: Result<number> = relevantPostDao.countBySource(url)
-
-        switch(result.kind) {
-            case ("success"): {
-                if (relevant) {
-                    if (result.value === 0) {
-                        let uuid = relevantPostDao.savePost(website, url, title, doc.textContent).value!
-
-                        postTweet({title, url, uuid})
-
-                    } else {
-                        // console.log("already saved", url)
-                    }
-                } else {
-                    // console.log("not relevant", url)
-                }
-                break;
-            }
-            case ("fail"): {
-                console.log("Error", result.message)
-                break;
-            }
-
+        } else {
+            console.log(4, feedType)
         }
-        await wait(5000);
+
+        for (const item of items) {
+            let title = item.title
+            let summary = item.summary
+            let url = item.url
+
+            let doc: any = domParser.parseFromString(summary, "text/html")
+            let relevant = isRelevant(title) || isRelevant(doc.textContent)
+            // TODO first extract all the url and check in one go, instead of one by one
+            let result: Result<number> = relevantPostDao.countBySource(url)
+
+            switch (result.kind) {
+                case ("success"): {
+                    if (relevant) {
+                        if (result.value === 0) {
+                            let uuid = relevantPostDao.savePost(website, url, title, doc.textContent).value!
+
+                            postTweet({title, url, uuid})
+
+                        } else {
+                            // console.log("already saved", url)
+                        }
+                    } else {
+                        // console.log("not relevant", url)
+                    }
+                    break;
+                }
+                case ("fail"): {
+                    console.log("Error", result.message)
+                    break;
+                }
+
+            }
+            await wait(5000);
+        }
+    } catch (e) {
+        console.log(`Error processing ${website} and ${rssLink}`)
     }
 }
 
